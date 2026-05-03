f83a271e37719510f2d487807d65b192e4a31333
jmhar3 <jmhar@protonmail.com>
2026-05-03 14:33:02 +1000

    implement routes + fonts

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,19 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import { MantineProvider } from "@mantine/core";
 
 import Home from "./pages/Home.tsx";
+import Features from "./pages/Features.tsx";
+import Gallery from "./pages/Gallery.tsx";
+import HireDetails from "./pages/HireDetails.tsx";
 import Events from "./pages/Events.tsx";
 import Contact from "./pages/Contact.tsx";
 
+// @ts-expect-error ignore type error
+import "@fontsource/gilda-display";
+// @ts-expect-error ignore type error
+import "@fontsource/alex-brush";
+
 import "@mantine/core/styles.css";
+import "./global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -15,7 +24,17 @@ createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/events" element={<Events />} />
+          <Route path="/features" element={<Features />} />
+
+          <Route path="/gallery/space" element={<Gallery />} />
+          <Route path="/gallery/art" element={<Gallery />} />
+
+          <Route path="/hire-details" element={<HireDetails />} />
+
+          <Route index path="/events" element={<Events />} />
+          <Route path="/events/workshops" element={<Events />} />
+          <Route path="/events/life-drawing" element={<Events />} />
+
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
