a51a7cde6a49ad0f31d45c2fb339e586ad9114cb
jmhar3 <jmhar@protonmail.com>
2026-05-03 14:32:16 +1000

    build Contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,29 @@
-import { Stack, Text } from "@mantine/core";
+import { Text, Title } from "@mantine/core";
+
+import PageLayout from "../components/PageLayout";
+import ContactForm from "../components/ContactForm";
+import Location from "../components/Location";
 
 function Contact() {
   return (
-    <Stack>
-      <Text>Get in touch</Text>
-      <Text>Find us at</Text>
-    </Stack>
+    <PageLayout>
+      <Title ta="center" size="3em" ff="Alex Brush" fw="normal">
+        Make an Inquiry
+      </Title>
+
+      <Text ta="center">
+        We’re available to answer any questions you may have so if you’re unsure
+        of anything get in touch or come in person for an open day on the first
+        Sunday of the month.
+        <br />
+        Shoot us an email at inquiries@xixistudiomelb.com | Call or text us at
+        0499999999
+      </Text>
+
+      <ContactForm />
+
+      <Location />
+    </PageLayout>
   );
 }
 
